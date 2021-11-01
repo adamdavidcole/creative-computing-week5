7a8c51abe45249eb5daa0716951d926bbb7c1693
Adam Cole <adamcole@Adams-MacBook-Air.local>
2021-11-01 22:57:33 +0000

    working projection, transform, and cleaned up code

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+/**
+ * CONSTANTS
+ */
 var fov = 500;
 
+/**
+ * SCENE SETUP AND EVENT HANDLERS
+ */
 var canvas = document.querySelector("canvas");
 var width = window.innerWidth;
 var height = window.innerHeight;
@@ -7,6 +13,33 @@ var context = canvas.getContext("2d");
 canvas.setAttribute("width", width);
 canvas.setAttribute("height", height);
 canvas.addEventListener("mousemove", getMouse, false);
+
+// move sphere with arrow keys
+document.addEventListener("keydown", function (event) {
+  const key = event.key; // "ArrowRight", "ArrowLeft", "ArrowUp", or "ArrowDown"
+  switch (key) {
+    case "ArrowLeft":
+      sphere.translate({ x: -5 });
+      break;
+    case "ArrowRight":
+      sphere.translate({ x: 5 });
+      // Right pressed
+      break;
+    case "ArrowUp":
+      sphere.translate({ y: -5 });
+      // Up pressed
+      break;
+    case "ArrowDown":
+      sphere.translate({ y: 5 });
+      // Down pressed
+      break;
+  }
+});
+
+/**
+ * UTILITY FUNCTIONS
+ */
+
 var mouseX = 0;
 var mouseY = 0;
 
@@ -14,142 +47,6 @@ function map(number, inMin, inMax, outMin, outMax) {
   return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
 }
 
-const cubeVertices = [
-  [0, 0, 0],
-  [1, 0, 0],
-  [1, 1, 0],
-  [0, 1, 0],
-  [0, 0, 1],
-  [1, 0, 1],
-  [1, 1, 1],
-  [0, 1, 1],
-];
-
-const cubeShape = new Shape({ verticies: cubeVertices });
-
-const sphere = new Sphere({ size: 200 });
-console.log("sphere", sphere, sphere.verticies);
-
-// const shapes = [];
-
-// var point = [];
-// var points = [];
-// var point3d = [];
-// var angleX = 0;
-// var angleY = 0;
-// var HALF_WIDTH = width / 2;
-// var HALF_HEIGHT = height / 2;
-
-// var x3d = 0;
-// var y3d = 0;
-// var z3d = 0;
-
-// var lastScale = 0;
-// var lastx2d = 0;
-// var lasty2d = 0;
-
-// // The below code creates a sphere of points
-// var dim = 100; // This is the number of rings
-// // Each ring has as many points as there are rings
-// // This is the spacing for each ring
-// var spacing = (Math.PI * 2) / dim;
-
-// // This is the total number of points
-// var numPoints = dim * dim;
-
-// // This is how big the sphere is.
-// var size = 200;
-
-// let numRenderedRings = dim;
-
-// // Now we build the sphere
-// for (var i = 0; i < numRenderedRings; i++) {
-//   // Calculate the depth spacing
-
-//   // To calculate the depth spacing, we divide our spacing in half
-//   // This is because otherwise, the cosine / sine waves will
-//   // oscillate positively and negatively
-//   // We only need the positive bit
-
-//   //     COOL MEGA SHAPE: come back to this later
-//   //      var z = size * Math.cos(spacing/2+1  * i);
-//   var z = size * Math.cos((spacing / 2) * i);
-
-//   // Calculate the size of the current ring
-//   //  COOL hourglass
-//   // var s = size * Math.sin(spacing  * i);
-//   // COOL tubes
-//   // var s = size * Math.sin(spacing* 10  * i);
-//   var s = size * Math.sin((spacing / 2) * i);
-
-//   // For each ring..
-
-//   for (var j = 0; j < dim; j++) {
-//     // ...create the next point in the circle at the current size s, at the current depth z
-
-//     var point = [Math.cos(spacing * j) * s, Math.sin(spacing * j) * s, z];
-
-//     // Add the point to the geometry.
-
-//     points.push(point);
-//   }
-// }
-
-// //
-// console.log(points.length);
-
-function draw() {
-  context.fillStyle = "rgb(0,0,0)";
-  context.fillRect(0, 0, width, height);
-
-  //   cubeShape.draw();
-  sphere.draw();
-
-  //   sphere.translate({ z: -1 });
-  sphere.scale({ x: -0.01, y: -0.01, z: -0.01 });
-  sphere.translate({ x: 0.01, z: -0.01, y: -0.01 });
-  sphere.rotate({ x: 0.01, y: 0.01, z: 0.01 });
-
-  requestAnimationFrame(draw);
-}
-
-requestAnimationFrame(draw);
-
-function rotateX(point3d, angleX) {
-  var x = point3d[0];
-  var z = point3d[2];
-
-  var cosRY = Math.cos(angleX);
-  var sinRY = Math.sin(angleX);
-
-  var tempz = z;
-  var tempx = x;
-
-  x = tempx * cosRY + tempz * sinRY;
-  z = tempx * -sinRY + tempz * cosRY;
-
-  point3d[0] = x;
-  point3d[2] = z;
-}
-
-function rotateY(point3d, angleY) {
-  var y = point3d[1];
-  var z = point3d[2];
-
-  var cosRX = Math.cos(angleY);
-  var sinRX = Math.sin(angleY);
-
-  var tempz = z;
-  var tempy = y;
-
-  y = tempy * cosRX + tempz * sinRX;
-  z = tempy * -sinRX + tempz * cosRX;
-
-  point3d[1] = y;
-  point3d[2] = z;
-}
-
-//here's our function 'getMouse'.
 function getMouse(mousePosition) {
   //for other browsers..
   if (mousePosition.layerX || mousePosition.layerX === 0) {
@@ -162,3 +59,41 @@ function getMouse(mousePosition) {
     mouseY = mousePosition.offsetY;
   }
 }
+
+/**
+ * SCENE LOGIC
+ */
+
+// create shapes
+const cubeVertices = [
+  [0, 0, 0],
+  [1, 0, 0],
+  [1, 1, 0],
+  [0, 1, 0],
+  [0, 0, 1],
+  [1, 0, 1],
+  [1, 1, 1],
+  [0, 1, 1],
+];
+const cubeShape = new Shape({ verticies: cubeVertices });
+const sphere = new Sphere({ size: 200 });
+
+function draw() {
+  // clear rect
+  context.fillStyle = "rgb(0,0,0)";
+  context.fillRect(0, 0, width, height);
+
+  // draw sphere
+  sphere.draw();
+
+  // update position
+  sphere.rotate({ z: 0.01, x: 0.01, y: 0.01 });
+  sphere.setScale({
+    x: map(mouseX, 0, width, -5, 5),
+    y: map(mouseY, 0, height, -5, 5),
+  });
+
+  requestAnimationFrame(draw);
+}
+
+requestAnimationFrame(draw);
--- shape.js
+++ shape.js
@@ -3,20 +3,22 @@ class Shape {
     this.verticies = verticies;
     this.rotatation = [0, 0, 0];
     this.translation = [0, 0, 0];
-    // this.translation = [width / 2, height / 2, 0];
     this.scaleFactor = [1, 1, 1];
   }
 
+  /**
+   * INCREMENTAL TRANSFORMS
+   */
   translate({ x, y, z }) {
     this.translation[0] += x || 0; // x translation
     this.translation[1] += y || 0; // y translation
-    this.translation[2] += z || 0; // ztranslation
+    this.translation[2] += z || 0; // z translation
   }
 
   rotate({ x, y, z }) {
-    this.rotatation[0] += x || 0;
-    this.rotatation[1] += y || 0;
-    this.rotatation[2] += z || 0;
+    this.rotatation[0] += x || 0; // x rotation
+    this.rotatation[1] += y || 0; // y rotation
+    this.rotatation[2] += z || 0; // z rotation
   }
 
   scale({ x, y, z }) {
@@ -25,14 +27,60 @@ class Shape {
     this.scaleFactor[2] += z || 1; // z scale
   }
 
+  /**
+   * SET TRANSFORMS DIRECTLY
+   */
+  setTranslation({ x, y, z }) {
+    const [currX, currY, currZ] = this.translation;
+    this.translation[0] = x || currX; // x translation
+    this.translation[1] = y || currY; // y translation
+    this.translation[2] = z || currZ; // z translation
+  }
+
+  setRotation({ x, y, z }) {
+    const [currX, currY, currZ] = this.rotatation;
+    this.rotatation[0] = x || currX; // x rotation
+    this.rotatation[1] = y || currY; // y rotation
+    this.rotatation[2] = z || currZ; // z rotation
+  }
+
+  setScale({ x, y, z }) {
+    const [currX, currY, currZ] = this.scaleFactor;
+    this.scaleFactor[0] = x || currX; // x scale
+    this.scaleFactor[1] = y || currY; // y scale
+    this.scaleFactor[2] = z || currZ; // z scale
+  }
+
+  /**
+   * ROTATION TRANSFORM OPERATIONS
+   */
   rotateX(vertex, angleX) {
+    if (angleX === 0) return vertex;
+
     let [x, y, z] = vertex;
 
-    var cosRY = Math.cos(angleX);
-    var sinRY = Math.sin(angleX);
+    var cosRX = Math.cos(angleX);
+    var sinRX = Math.sin(angleX);
 
+    var tempy = y;
     var tempz = z;
+
+    y = tempy * cosRX - tempz * sinRX;
+    z = tempy * sinRX + tempz * cosRX;
+
+    return [x, y, z];
+  }
+
+  rotateY(vertex, angleY) {
+    if (angleY === 0) return vertex;
+
+    let [x, y, z] = vertex;
+
+    var cosRY = Math.cos(angleY);
+    var sinRY = Math.sin(angleY);
+
     var tempx = x;
+    var tempz = z;
 
     x = tempx * cosRY + tempz * sinRY;
     z = tempx * -sinRY + tempz * cosRY;
@@ -40,222 +88,76 @@ class Shape {
     return [x, y, z];
   }
 
-  rotateY(vertex, angleY) {
+  rotateZ(vertex, angleZ) {
+    if (angleZ === 0) return vertex;
+
     let [x, y, z] = vertex;
 
-    var cosRX = Math.cos(angleY);
-    var sinRX = Math.sin(angleY);
+    var cosRZ = Math.cos(angleZ);
+    var sinRZ = Math.sin(angleZ);
 
-    var tempz = z;
+    var tempx = x;
     var tempy = y;
 
-    y = tempy * cosRX + tempz * sinRX;
-    z = tempy * -sinRX + tempz * cosRX;
+    x = tempx * cosRZ - tempy * sinRZ;
+    y = tempx * sinRZ + tempy * cosRZ;
 
     return [x, y, z];
   }
 
-  getWorkingTransform(verticies) {
+  /**
+   * 3D TRANSFORMATION AND PROJECTION
+   */
+  getProjectionTransform(verticies) {
     return verticies.map((vertex) => {
-      //   z -= 1.0;
-      //   if (z < -fov) z = fov;
+      // get transform values
       const [translationX, translationY, translationZ] = this.translation;
       const [rotationX, rotationY, rotationZ] = this.rotatation;
       const [scaleX, scaleY, scaleZ] = this.scaleFactor;
 
+      // rotate the shape
       let rotatedVertex = [...vertex];
       rotatedVertex = this.rotateX(rotatedVertex, rotationX);
       rotatedVertex = this.rotateY(rotatedVertex, rotationY);
 
       let [x, y, z] = rotatedVertex;
 
+      // scale and translate the shape
+      x = scaleX * x + translationX;
+      y = scaleY * y + translationY;
       z = scaleZ * z + translationZ;
-      //   if (z < -fov) {
-      //     z = 0;
-      //     vertex[2] = z;
-      //   }
 
+      // get 3D projection scale factor
       const projectionScale = fov / (fov + z);
 
-      const projectionX =
-        projectionScale * scaleX * x + translationX + width / 2;
-      const projectionY =
-        projectionScale * scaleY * y + translationY + height / 2;
+      // project the shape into 3D and translate to world center
+      const projectionX = projectionScale * x + width / 2;
+      const projectionY = projectionScale * y + height / 2;
 
+      // return projected/transformed vertex with projectionScale as extra value for use later
       return [projectionX, projectionY, z, projectionScale];
     });
   }
 
-  getModelTransform(verticies) {
-    const transformedVertices = verticies.map(([x, y, z], i) => {
-      if (i == 100) console.log("mapping", { x, y, z });
-
-      const [translateX, translateY, translateZ] = this.translation;
-      const [scaleX, scaleY, scaleZ] = this.scaleFactor;
-      const [rotationX, rotationY, rotationZ] = this.rotatation;
-
-      var projectionScale = fov / (fov + z * scaleZ);
-
-      const scaledVertex = [scaleX * x, scaleY * y, scaleZ * z];
-
-      //   const rotatedVertex = scaledVertex;
-      const rotatedVertex = this.rotateY(
-        this.rotateX(scaledVertex, rotationX),
-        rotationY
-      );
-
-      const translatedVertex = [
-        rotatedVertex[0] + translateX,
-        rotatedVertex[1] + translateY,
-        rotatedVertex[2] + translateZ,
-      ];
-
-      return [...translatedVertex, projectionScale];
-      //   return [
-      //     scaleX * projectionScale * x + translateX,
-      //     scaleY * projectionScale * y + translateY,
-      //     scaleZ * z + translateZ,
-      //     projectionScale,
-      //   ];
-    });
-
-    return transformedVertices;
-  }
-
-  getProjectionTransform(transformedVertices) {
-    const projectedVertices = transformedVertices.map(([x, y, z], i) => {
-      if (i == 100) console.log("transformed", { x, y, z, scale });
-      //   if (z3d < -fov) z3d += fov;
-      var scale = fov / (fov + z);
-      return [x * scale + width / 2, y * scale + height / 2, z, scale];
-    });
-
-    return projectedVertices;
-
-    // for (var i = 0; i < numRenderedRings * numRenderedRings; i++) {
-    //   point3d = points[i];
-    //   z3d = point3d[2];
-
-    //   // This is the speed of the z
-    //   // It moves the points forwards in space
-    //   // We don't need it for the pure rotate
-    //   z3d -= 1.0;
-
-    //   // Check that the points aren't disappearing into space and if so push them back
-    //   // This also stops them stretching
-    //   // When they get too close
-    //   if (z3d < -fov) z3d += 2 * fov;
-
-    //   point3d[2] = z3d;
-
-    //   // Calculate the rotation
-
-    //   //     rotateX(point3d,angleX/4);
-    //   //     rotateY(point3d,angleY/4);
-
-    //   // Get the point in position
-
-    //   x3d = point3d[0];
-    //   y3d = point3d[1];
-    //   z3d = point3d[2];
-    //   // Convert the Z value to a scale factor
-    //   // This will give the appearance of depth
-    //   var scale = fov / (fov + z3d);
-
-    //   // Store the X value with the scaling
-    //   // FOV is taken into account
-    //   // (just pushing it over to the left a bit too)
-    //   var x2d = x3d * scale + HALF_WIDTH;
-
-    //   // Store the Y value with the scaling
-    //   // FOV is taken into account
-
-    //   var y2d = y3d * scale + HALF_HEIGHT;
-
-    //   // Draw the point
-
-    //   // Set the size based on scaling
-    //   context.lineWidth = scale;
-
-    //   context.strokeStyle = "rgb(255,255,255)";
-    //   context.beginPath();
-    //   context.moveTo(x2d, y2d);
-    //   context.lineTo(x2d + scale, y2d);
-    //   context.stroke();
-    // }
-  }
-
+  /**
+   * DRAW SHAPE
+   */
   draw() {
-    // const transformedVertices = this.getModelTransform(this.verticies);
-    // const projectedVertices = this.getProjectionTransform(transformedVertices);
-    const projectedVertices = this.getWorkingTransform(this.verticies);
+    // get 3D projected and transformed model
+    const projectedVertices = this.getProjectionTransform(this.verticies);
 
-    projectedVertices.forEach(([x, y, z, scale], i) => {
-      context.lineWidth = scale;
+    // draw each vertex
+    projectedVertices.forEach(([x, y, z, projectionScale], i) => {
+      context.lineWidth = projectionScale;
 
-      //   if (i == 100) console.log({ x, y, z, scale });
+      // if point is beyond field of view, do not render it
+      if (z < -fov) return;
 
       context.strokeStyle = "rgb(255,255,255)";
       context.beginPath();
       context.moveTo(x, y);
-      context.lineTo(x + scale, y);
+      context.lineTo(x + projectionScale, y);
       context.stroke();
     });
-
-    // WORKING ORIGINAL
-    // const points = this.verticies;
-
-    // for (var i = 0; i < points.length; i++) {
-    //   const point3d = points[i];
-    //   //   let z3d = point3d[2];
-
-    //   // This is the speed of the z
-    //   // It moves the points forwards in space
-    //   // We don't need it for the pure rotate
-
-    //   // Check that the points aren't disappearing into space and if so push them back
-    //   // This also stops them stretching
-    //   // When they get too close
-    //   //              z3d -= 1.0;
-
-    //   //   if (z3d < -fov) z3d += 0;
-
-    //   //   point3d[2] = z3d;
-
-    //   // Calculate the rotation
-
-    //   //     rotateX(point3d,angleX);
-    //   //     rotateY(point3d,angleY);
-
-    //   // Get the point in position
-
-    //   let x3d = point3d[0];
-    //   let y3d = point3d[1];
-    //   let z3d = point3d[2];
-    //   // Convert the Z value to a scale factor
-    //   // This will give the appearance of depth
-    //   var scale = fov / (fov + z3d);
-
-    //   // Store the X value with the scaling
-    //   // FOV is taken into account
-    //   // (just pushing it over to the left a bit too)
-    //   var x2d = x3d * scale + width / 2;
-
-    //   // Store the Y value with the scaling
-    //   // FOV is taken into account
-
-    //   var y2d = y3d * scale + height / 2;
-
-    //   // Draw the point
-
-    //   // Set the size based on scaling
-    //   context.lineWidth = scale;
-
-    //   context.strokeStyle = "rgb(255,255,255)";
-    //   context.beginPath();
-    //   context.moveTo(x2d, y2d);
-    //   context.lineTo(x2d + scale, y2d);
-    //   context.stroke();
-    // }
   }
 }
